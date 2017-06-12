29d6cc60d9c75bd48665adf3a7cfbde8178c8729
DesktopMauri <maurikiller732@gmail.com>
2017-06-12 19:56:25 -0400

    V0.0.2 Suma

---
+++ Suma/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by DesktopMauri on 12/06/2017.
+ */
+public class Main {
+    public static void main(String args[]){
+        Scanner leer=new Scanner(System.in);
+        int suma=leer.nextInt()+leer.nextInt();
+        System.out.println(suma);
+    }
+}
