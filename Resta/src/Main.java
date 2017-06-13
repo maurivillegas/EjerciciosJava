6fa6c397ae5541b9c5535847c674d5f2faa11d06
LaptopMauri <maurikiller732@gmail.com>
2017-06-12 20:11:59 -0400

    v0.0.6 resta

---
+++ Resta/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by LaptopMauri on 12/06/2017.
+ */
+public class Main {
+    public static void main(String args[]){
+        Scanner leer = new Scanner(System.in);
+        int resta=leer.nextInt()-leer.nextInt();
+        System.out.println(resta);
+    }
+}
